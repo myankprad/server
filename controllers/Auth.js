5c0165b7cedcad6474edcae4b3f83ce098beaf40
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-09-29 14:55:39 +0530

    cc

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,10 @@
 const User = require("../models/User")
 const OTP = require("../models/OTP")
-const otpGenerator = require("otp-generator")
+const otpGenerator = require("otp-generator");
+const Profile = require("../models/Profile");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 
 // send otp
@@ -62,9 +66,10 @@ exports.sendOTP = async(req, res)=>{
 // signup
 
 exports.signUp = async(req, res)=>{
-    // data fetch
+   try {
+     // data fetch
      
-    const {
+     const {
         firstName,
         lastName,
         email,
@@ -123,11 +128,125 @@ exports.signUp = async(req, res)=>{
     
     // hash password
 
+    const hashedPassword = await bcrypt.hash(password, 10)
+
     
     // entry in db
 
+    const profileDetails = await Profile.create({
+        gender:null,
+        dateOfBirth:null,
+        about:null,
+        contactNumber:null
+    })
+     
+    const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        contactNumber,
+        password: hashedPassword,
+        accountType,
+        additionalDetails: profileDetails._id,
+        image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`
+    })
+    // res return 
+    return res.status(200).json({
+        success:true,
+        message:"User is registered successfully",
+        user,
+    })
+   } catch (error) {
+    console.log(error),
+        res.status(500).json({
+        success:false,
+        message: "user can not be registered",
+
+    })
+   }
 }
 
 // login
 
-// reset password
+exports.login = async(req, res)=>{
+    try {
+        // get data from body
+
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(403).json({
+                success:false,
+                message: "All fields are required, please try again",
+            })
+        }
+
+        // user check exist or not
+        const user = await User.findOne({email}).populate("additionalDetails")
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message: "User is not registered, please sign in"
+            })
+        }
+
+        // generate token after check password
+
+        if(await bcrypt.compare(password, user.password)){
+            const payload = {
+                email: user.email,
+                id: user._id,
+                accountType: user.accountType
+            }
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "2h", 
+            }) 
+            user.token = token;
+            user.password = undefined;  
+            
+              // create cookie and send response
+
+        const options ={
+            expires: new Date(Date.now() + 3*34*60*60*1000),
+            httpOnly: true
+        }
+
+        res.cookie("token", token, options).status(200).json({
+            success: true,
+            token,
+            user,
+            message: "Login successfully"
+        })
+        } else{
+            return res.status(401).json({
+                success: false,
+                message: "Password did not matched"
+            })
+        }
+
+      
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message: "Login failure, please try again"
+        })
+    }
+}
+
+// change password
+
+exports.changePassword = async(req, res)=>{
+    // get data from body
+
+    // get oldpassword newpassword confirmnewpassword
+
+    // validation
+
+    // update pwd in db
+
+    // send mail password updated
+
+    // return response
+}
